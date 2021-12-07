a153cb655519d857d6d0e5dfc42744659d30d45b
AvivBenY <benyedidia123@gmail.com>
2021-12-07 18:07:14 +0200

    added the projects instructions

---
+++ HangMan Project/ImagesOnScreen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace HangMan_Project_Aviv
+{
+    class ImagesOnScreen
+    {
+        public Image _image { get; set; }
+        private double _width;
+        private double _height;
+        private double _top;
+        private double _left;
+        private string _uri;
+
+        //Constructor
+        public ImagesOnScreen(double width, double height, double top, double left, string uri)
+        {
+            _image = new Image();
+            _width = width;
+            _height = height;
+            _top = top;
+            _left = left;
+            _uri = uri;
+            _image.Source = new BitmapImage(new Uri($"ms-appx://{uri}"));
+            _image.Width = width;
+            _image.Height = height;
+            Canvas.SetLeft(_image, left);
+            Canvas.SetTop(_image, top);
+        }
+
+        //Takes image
+        public virtual Image GetImage()
+        {
+            return _image;
+        }
+    }
+}
---
+++ HangMan Project/Words Difficulty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HangMan_Project_Aviv
+{
+    public class Words_Difficulty
+    {
+        Random rand = new Random();        
+        static public string chosenWord;
+
+        //Randomly takes word out of easy array's
+        #region Word Choosing
+        public void TakeWordOutOfEasyArr(string[] _easyWordsArr)
+        {
+            chosenWord = _easyWordsArr[rand.Next(0, _easyWordsArr.Length)];
+        }
+
+        //Randomly takes word out of easy array's
+        public void TakeWordOutOfHardArr(string[] hardWordsArr)
+        {
+            chosenWord = hardWordsArr[rand.Next(0, hardWordsArr.Length)];
+        }
+        #endregion
+    }
+}
